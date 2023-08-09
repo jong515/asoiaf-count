c393ddcfdaed49761b6b21dc3a85b727c9b171e3
jong515 <jongoh515@gmail.com>
2023-08-09 19:24:36 +0800

    Changed to make it a function instead

--- days_since_dwd.py
+++ days_since_dwd.py
@@ -1,11 +1,12 @@
 import datetime
 
-current_date=datetime.date.today()
+def check_days_since_dwd():
+    current_date=datetime.date.today()
+    dwd_date=datetime.date(2011,7,12)
+    days_since=(current_date-dwd_date).days
+    return days_since
 
-dwd_date=datetime.date(2011,7,12)
 
-days_since=(current_date-dwd_date).days
-
-print(days_since)
+print(check_days_since_dwd())
 
 
