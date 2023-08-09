4eb158b4f84870f6016e5e1d5a42e133e77eba06
jong515 <jongoh515@gmail.com>
2023-08-09 19:29:11 +0800

    Added some comments describing the function and

--- days_since_dwd.py
+++ days_since_dwd.py
@@ -1,12 +1,13 @@
 import datetime
 
+
 def check_days_since_dwd():
-    current_date=datetime.date.today()
-    dwd_date=datetime.date(2011,7,12)
-    days_since=(current_date-dwd_date).days
-    return days_since
+    current_date = datetime.date.today() # finds current date using datetime
+    dwd_date = datetime.date(2011,7,12) # finds date of release for ADWD
+    days_since = (current_date-dwd_date).days # calculates the number of days since its release
+    return days_since 
 
 
-print(check_days_since_dwd())
+print(f"It has been {check_days_since_dwd()} days since A Dance With Dragons was released.") 
 
 
