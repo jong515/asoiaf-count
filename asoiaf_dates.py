48c0403a7d07a231a1dee78de3056897cf9ad135
jong515 <jongoh515@gmail.com>
2023-08-09 22:15:56 +0800

    Added the other books release dates

--- asoiaf_dates.py
+++ asoiaf_dates.py
@@ -1,5 +1,18 @@
 import datetime
 
-agot_date = datetime.date(1996,1,8) # finds date of release for ADWD
+agot_date = datetime.date(1996,1,8) 
+# finds date of release for AGOT
+
+acok_date = datetime.date(1998,11,16) 
+# finds date of release for ACOK
+
+asos_date = datetime.date(2000,8,8) 
+# finds date of release for ASOS
+
+affc_date = datetime.date(2005,10,17) 
+# finds date of release for AFFC
 
 print(agot_date)
+print(acok_date)
+print(asos_date)
+print(affc_date)
