666746d08b6a7eba77010dac83a86f93bcfa759f
jong515 <jongoh515@gmail.com>
2023-08-09 18:26:22 +0800

    Changed the .days position

--- days_since_dwd.py
+++ days_since_dwd.py
@@ -4,8 +4,8 @@ current_date=datetime.date.today()
 
 dwd_date=datetime.date(2011,7,12)
 
-days_since=current_date-dwd_date
+days_since=(current_date-dwd_date).days
 
-print(days_since.days)
+print(days_since)
 
 
