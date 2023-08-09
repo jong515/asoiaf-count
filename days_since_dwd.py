3fa43a1477447bea908db34997c234accbc5fe9b
jong515 <jongoh515@gmail.com>
2023-08-09 21:15:21 +0800

    Added code to include conversion of the total number of days to years,months,days.
    Additionally, changing the message if the number of
    days/months are only 1 to have better grammar.

--- days_since_dwd.py
+++ days_since_dwd.py
@@ -1,13 +1,31 @@
 import datetime
 
-dwd_date = datetime.date(2011,7,12) # finds date of release for ADWD
-
-def check_days_since_dwd():
-    current_date = datetime.date.today() # finds current date using datetime
-    days_since = (current_date-dwd_date).days # calculates the number of days since its release
-    return days_since 
+dwd_date = datetime.date(2011,7,12) 
+current_date = datetime.date.today()
 
 
-print(f"It has been {check_days_since_dwd()} days since A Dance With Dragons was released.") 
+total_days_since = (current_date-dwd_date).days 
+# calculates the number of days since its release
 
+years = total_days_since // 365
+months = (total_days_since - years * 365) // 30
+days = (total_days_since - years * 365 - months * 30)
+# converts the number of days into years, months, days
 
+msg = ( f"It has been {total_days_since} days since A Dance With Dragons was released. \n\n" 
+        f"OR \n\n{years} years, {months} months, {days} days since A Dance With Dragons was released.")
+
+""" Creates a string message to print out, then checks if its a single month or day and changes the message 
+accordingly """
+
+if months == 1:
+    msg = ( f"It has been {total_days_since} days since A Dance With Dragons was released. \n\n" 
+        f"OR \n{years} years, {months} month, {days} days since A Dance With Dragons was released.")
+if days == 1:
+    msg = ( f"It has been {total_days_since} days since A Dance With Dragons was released. \n\n" 
+        f"OR \n\n{years} years, {months} months, {days} day since A Dance With Dragons was released.")
+if months == 1 and days == 1:
+    msg = ( f"It has been {total_days_since} days since A Dance With Dragons was released. \n\n" 
+        f"OR \n\n{years} years, {months} month, {days} day since A Dance With Dragons was released.")
+
+print(msg)
